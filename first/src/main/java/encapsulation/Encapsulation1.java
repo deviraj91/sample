8bd5a1deed817e63582a7732d151ef069ced4700
devir <devir@192.168.0.6>
2024-05-17 23:49:54 +0530

    encapsulation pushed

---
+++ first/src/main/java/encapsulation/Encapsulation1.java
@@ -0,0 +1,12 @@
+package encapsulation;
+
+public class Encapsulation1 {
+
+	public static void main(String[] args) {
+		Encapsulationex obj=new Encapsulationex();
+		
+		obj.setname("hai");
+		obj.getname();
+	}
+
+}
---
+++ first/src/main/java/encapsulation/Encapsulationex.java
@@ -0,0 +1,20 @@
+package encapsulation;
+
+public class Encapsulationex {
+private String name;
+	public void getname()
+	{
+		System.out.println(name);
+	}
+	
+	public void setname(String name)
+	{
+	this.name=name;	
+	}
+	}
+
+
+
+	
+
+
